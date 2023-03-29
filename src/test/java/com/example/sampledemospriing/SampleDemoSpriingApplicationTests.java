3bad6ebde8a228c18eeea3bd0711589be1d95c2c
Insecupa <abhijeetrayapeddi@gmail.com>
2023-03-29 23:44:08 +0530

    Initial Commit

---
+++ src/test/java/com/example/sampledemospriing/SampleDemoSpriingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampledemospriing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDemoSpriingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
